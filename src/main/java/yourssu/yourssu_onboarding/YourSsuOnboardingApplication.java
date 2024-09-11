6be50141b81cdbe400b7adc975c120bc4c95bd2c
SangWookie <swcube520@gmail.com>
2024-09-11 20:39:51 +0900

    Initial commit

---
+++ src/main/java/yourssu/yourssu_onboarding/YourSsuOnboardingApplication.java
@@ -0,0 +1,13 @@
+package yourssu.yourssu_onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourSsuOnboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourSsuOnboardingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yourssu/yourssu_onboarding/YourSsuOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package yourssu.yourssu_onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourSsuOnboardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
